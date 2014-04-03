c484f6e0ad8ad0281434d43d58f83c39b9d1ffb7
Aladine <alazaiane@yahoo.fr>
2014-04-03 23:26:45 +0200

    premiers pas vers le signup et home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 AppUser::Application.routes.draw do
+  root 'static_pages#home'
+  get "users/new"
+
+  match "/signup", to: "users#new", via: "get"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
