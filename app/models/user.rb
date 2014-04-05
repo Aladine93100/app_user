512157ba5f5a6c62c3056e72e65eab8a72d918dd
Aladine <alazaiane@yahoo.fr>
2014-04-05 18:27:21 +0200

    ajout de test et validation du secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	validates(:name, :city, :age, presence: true)
+	validates :password , length: { minimum: 6 }
+	has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example user", city: "Example user", age: "Example user") }
+	before do 
+		@user = User.new(name: "Example user", city: "Example user", age: "Example user",
+			password: "foobar", password_confirmation: "foobar")
+	end
 
 	subject{ @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:city) }
 	it { should respond_to(:age) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
 	it { should be_valid }
+	it { should respond_to(:authenticate) }
 
 	describe "when name is not present" do
 		before { @user.name = "" }
@@ -25,5 +32,33 @@ describe User do
 		before { @user.age = ""}
 		it { should_not be_valid }
 	end
+
+	describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", age: "Example User", city: "Example User",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+  	before { @user.save }
+  	let(:found_user) { User.find_by(name: @user.name) }
+
+  	describe "with valid password" do
+  		it { should eq found_user.authenticate(@user.password) }
+  	end
+
+  	describe "with invalid password" do
+  		let(:user_user_for_invalid_password) { found_user.authenticate("invalid") }
+
+  		it { should_not eq user_user_for_invalid_password }
+  		specify { expect(user_user_for_invalid_password).to be_false }
+  	end
+  end
 end
- 
